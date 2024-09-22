86935ddc6655f88403d5556f81572c60c82ce50f
Abdallah <abdallahbeedo855@gmail.com>
2024-09-22 20:23:39 +0300

    end

--- client/redux/API/api.js
+++ client/redux/API/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
- baseQuery: fetchBaseQuery({ baseUrl: "back-end-dashboard.vercel.app" }),
+ baseQuery: fetchBaseQuery(),
  reducerPath: "adminApi",
  tagTypes: [
   "User",
