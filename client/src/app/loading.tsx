ba48462388052752223ad143662fb857c39c61a3
Abdallah <abdallahbeedo855@gmail.com>
2024-09-30 17:37:33 +0300

    updating UI

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const loading = () => {
  return (
   <div className="my-24 min-h-[80vh] text-gray-400 flex justify-center items-center gap-4">
-   loading...{" "}
-   <div className="border-t-4  border-gray-400 rounded-full w-16 h-16 animate-spin"></div>
+   <h2 className="text-yellow-500 text-center">Loading</h2>
+   <div className="border-t-4  border-yellow-500 rounded-full w-16 h-16 animate-spin"></div>
   </div>
  );
 };
