7f716462c6d8982a55f89c9a3a88d0bf44ade5ca
Abdullah <117304875+Abdullah-Radwan1@users.noreply.github.com>
2025-07-24 07:01:06 +0300

    Update zod.schema.js

--- server/utils/zod.schema.js
+++ server/utils/zod.schema.js
@@ -15,3 +15,4 @@ export const loginSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
+//rebuild
