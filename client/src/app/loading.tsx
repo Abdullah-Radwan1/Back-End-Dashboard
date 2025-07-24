051e3d6b2e362c2f7e5a792345b49f38edbb9b63
abdullah <abdallahbeedo855@gmail.com>
2025-07-24 06:30:46 +0300

    finish

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -1,7 +1,7 @@
 import { Loader } from "lucide-react";
 import React from "react";
 
-export const Loading = () => {
+const Loading = () => {
   return (
     <div className=" text-gray-400 flex justify-center  h-full items-center gap-4">
       <Loader className="animate-spin justify-center items-center" />
