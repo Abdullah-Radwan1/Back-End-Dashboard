502ca6ee7d6bfe063d673f0278401423f3272bc3
abdullah <abdallahbeedo855@gmail.com>
2025-08-12 18:55:54 +0300

    fixing start up redirect

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,11 +3,5 @@ import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  const isLoggedIn = cookieStore.has("auth-token"); // Replace with your auth cookie/token logic
-  if (isLoggedIn) {
-    redirect("/main/dashboard"); // Redirect to main if logged in
-  } else {
-    redirect("/auth/login"); // Redirect to auth if not logged in
-  }
+  redirect("/main/dashboard"); // Redirect to main if logged in
 }
